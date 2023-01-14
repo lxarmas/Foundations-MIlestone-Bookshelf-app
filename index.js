00b618a46c72cee68b9caacc5c98c4acdaccbade
alejandro armas <lxarmas@gmail.com>
2023-01-13 19:58:20 -0800

    Add files via upload
    this is the second draft, include on click functions on the side cover of the book

--- index.js
+++ index.js
@@ -244,7 +244,36 @@ function myFunction() {
 }
 
 
-document.getElementById('The Life Of Tarzan').onclick = bookClick;
-function bookClick(clicked){
- Tarzan.innerHTML = 'Button clicked,"this book is the shit"'
+
+function myFunction2() {
+  document.getElementById("demo").innerHTML = "Tarzan is a series of 24 adventure novels written by Edgar Rice Burroughs (1875–1950) and published between 1912 and 1966";
+}
+
+
+function myFunction3() {
+  document.getElementById("demo3").innerHTML = "is a novel by the Russian author Fyodor Dostoevsky. It was first published in the literary journal The Russian Messenger in twelve monthly installments during 1866.";
+}
+
+function myFunction4() {
+  document.getElementById("demo4").innerHTML = "is a novel by Bram Stoker, published in 1897. An epistolary novel, the narrative is related through letters, diary entries, and newspaper articles. It has no single protagonist.";
+}
+
+function myFunction5() {
+  document.getElementById("demo5").innerHTML = " the novel includes little dialogue and almost no direct action; most of it is written as thoughts and observations";
+}
+
+function myFunction6() {
+  document.getElementById("demo6").innerHTML = "The novel was influenced by Romanticism and Gothic fiction.";
+}
+
+function myFunction7() {
+  document.getElementById("demo7").innerHTML = "A pragmatic U.S. Marine observes the dehumanizing effects the Vietnam War has on his fellow recruits from their brutal boot camp";
+}
+
+function myFunction8() {
+  document.getElementById("demo8").innerHTML = "The life of a little boy around Lima in the 1930s.";
+}
+
+function myFunction9() {
+  document.getElementById("demo9").innerHTML = "Julio Cortazar's Masterpiece";
 }
